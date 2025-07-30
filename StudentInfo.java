aeace6eeeeb1505af2b33e0f73d5d77c3274da0a
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2025-07-31 00:51:09 +0800

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+    String studentID;
+    String firstName;
+    String lastName;
+    String course;
+    String section;
+
+    int midtermExamScore;
+    int finalExamScore;
+    int projectScore;
+    int attendancePercentage;
+}
