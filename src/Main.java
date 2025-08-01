4bfcb72c25aa2923765c0b37fd2c997265395653
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2025-08-01 10:19:26 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        int totalNumber = 0;
+        Scanner scanner = new Scanner (System.in);
+
+        for (int number = 1; number <= 5; number++) {
+            System.out.println("Enter number" + " " + number + ":");
+            totalNumber = scanner.nextInt();
+            totalNumber += number;
+        }
+            System.out.println("Total Number: " + totalNumber);
+
     }
 }
